0f1683f4988be5e7f51e7c293377070a4d7bfd57
isekai <brian1689878638@gmail.com>
2021-05-09 06:57:29 +0800

    Use postgres uuid default function instead

--- models/identity.go
+++ models/identity.go
@@ -15,15 +15,6 @@ type Identity struct {
 	CreatedAt  time.Time
 }
 
-func (i *Identity) BeforeCreate(tx *gorm.DB) error {
-	i.UUID = uuid.New()
-	return nil
-}
-
-func DefaultUUID() uuid.UUID {
-	return uuid.New()
-}
-
 func (i *Identity) AfterFind(tx *gorm.DB) error {
 	err := tx.Model(i).Update("last_online", time.Now()).Error
 	return err
